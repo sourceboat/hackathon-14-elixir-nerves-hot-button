567e2c98e8d41fc4e46b2c6052ed0d6973ab00d0
Hauke <hauke@sourceboat.com>
2022-02-14 14:32:42 +0100

    Create new nerves project

---
+++ lib/hot_button.ex
@@ -0,0 +1,18 @@
+defmodule HotButton do
+  @moduledoc """
+  Documentation for HotButton.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HotButton.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hot_button_test.exs
@@ -0,0 +1,8 @@
+defmodule HotButtonTest do
+  use ExUnit.Case
+  doctest HotButton
+
+  test "greets the world" do
+    assert HotButton.hello() == :world
+  end
+end
